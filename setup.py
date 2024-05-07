a76ee415f3cae098ab62f7b5a8d15cdfb7899400
nikhiljose7 <nikhiljose168@gmail.com>
2024-05-07 17:02:15 +0530

    setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Nikhil',
+    author='nikhiljose7',
     author_email='nikhiljose168@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
